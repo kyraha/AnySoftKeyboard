43a0feb3e2f0bc69340569814638b06c82d21535
Mikhail Kyraha <1049133+kyraha@users.noreply.github.com>
2025-08-04 22:26:23 -0500

    Sakha folder created in addons with a few initial files

---
+++ addons/languages/sakha/pack/src/main/java/com/anysoftkeyboard/languagepack/sakha/PackBroadcastReceiver.java
@@ -0,0 +1,5 @@
+package com.anysoftkeyboard.languagepack.sakha;
+
+import com.anysoftkeyboard.addon.PackBroadcastReceiverBase;
+
+public class PackBroadcastReceiver extends PackBroadcastReceiverBase {}
